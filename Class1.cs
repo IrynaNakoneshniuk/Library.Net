d78710b287ec390070d2ca992617268502bfad12
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-08-23 20:01:46 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDLL
+{
+    public static  class Class1
+    {
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Min(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ SaveReadXMLcs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace UserDLL
+{
+    public static class SaveReadXML
+    {
+        public static void SerializeObject<T>(string filepath, T serializableObject)
+        {
+            if (serializableObject == null) { return; }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, serializableObject);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(filepath);
+                    stream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        public static T DeSerializeObject<T>(string filepath)
+        {
+            T objectOut = default(T);
+
+            if (!System.IO.File.Exists(filepath)) return objectOut;
+
+            try
+            {
+                string attributeXml = string.Empty;
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(filepath);
+                string xmlString = xmlDocument.OuterXml;
+
+                using (StringReader read = new StringReader(xmlString))
+                {
+                    Type outType = typeof(T);
+
+                    XmlSerializer serializer = new XmlSerializer(outType);
+                    using (XmlReader reader = new XmlTextReader(read))
+                    {
+                        objectOut = (T)serializer.Deserialize(reader);
+                        reader.Close();
+                    }
+
+                    read.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            return objectOut;
+        }
+    }
+    [Serializable]
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Email}";
+        }
+    }
+    [Serializable]
+    public class Manager
+    {
+        public string Name { get; set; }
+
+        public User FavoriteEmployee { get; set; }
+    }
+}
